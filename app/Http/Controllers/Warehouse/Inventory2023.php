2a05259da4bfd2016cce6fa595c007a87a1135fd
Alonso Jiménez <ajimmenezz@gmail.com>
2023-12-19 10:47:48 -0600

    Filtrar almacenes

--- app/Http/Controllers/Warehouse/Inventory2023.php
+++ app/Http/Controllers/Warehouse/Inventory2023.php
@@ -13,7 +13,9 @@ class Inventory2023 extends Controller
     {
         return view('warehouse.inventory2023.index', [
             'title_content' => $this->title_content('warehouse.inventory2023.index'),
-            'warehouses' => Warehouses::where('STATUS', 'A')->orderBy('DESCR')->get(),
+            'warehouses' => Warehouses::where('STATUS', 'A')
+            ->whereIN('CVE_ALM', [1,4,10,101])
+            ->orderBy('DESCR')->get(),
         ]);
     }
 }
